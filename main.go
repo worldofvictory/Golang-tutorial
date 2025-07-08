6c4be7068c1b8496827b9e98c40af5ea8d8e6772
worldofvictory <133643234+worldofvictory@users.noreply.github.com>
2025-07-08 12:34:43 +0200

    simply booking-app

--- main.go
+++ main.go
@@ -7,8 +7,10 @@ func main() {
 	var firstName string
 	var lastName string
 	var email string
-	var userTickets = 2
+	var remainingTickets int = 50
+	var userTickets int
 	//ask user for their name
+	fmt.Printf("Tickets remaining: %v\n", remainingTickets)
 
 	fmt.Printf("Enter your first name: ")
 	fmt.Scan(&firstName)
@@ -16,8 +18,14 @@ func main() {
 	fmt.Printf("Enter your last name: ")
 	fmt.Scan(&lastName)
 
+	fmt.Printf("How many tickets would you like to book? ")
+	fmt.Scan(&userTickets)
+
 	fmt.Printf("Enter your email: ")
 	fmt.Scan(&email)
+
 	fmt.Printf("User %v has booked %v tickets.\n", firstName, userTickets)
 	fmt.Printf("Thank you %v %v for booking %v tickets. A confirmation email has been sent to %v.\n", firstName, lastName, userTickets, email)
+	remainingTickets = remainingTickets - userTickets
+	fmt.Printf("Tickets remaining: %v\n", remainingTickets)
 }
