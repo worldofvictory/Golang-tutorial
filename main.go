92f53763904ea9d6cba166baf2d3ff744216b278
worldofvictory <133643234+worldofvictory@users.noreply.github.com>
2025-07-09 12:50:14 +0200

    loop if

--- main.go
+++ main.go
@@ -1,6 +1,9 @@
 package main
 
-import "fmt"
+import (
+	"fmt"
+	"strings"
+)
 
 func main() {
 	var firstName string
@@ -8,8 +11,8 @@ func main() {
 	var email string
 	var remainingTickets int = 50
 	var userTickets int
+	bookings := []string{} //deve stare fuori  dal for, //altrimenti viene resettato ad ogni iterazione
 	for {
-		bookings := []string{}
 
 		fmt.Printf("Tickets remaining: %v\n", remainingTickets)
 
@@ -19,11 +22,16 @@ func main() {
 		fmt.Printf("Enter your last name: ")
 		fmt.Scan(&lastName)
 
+		fmt.Printf("Enter your email: ")
+		fmt.Scan(&email)
+
 		fmt.Printf("How many tickets would you like to book? ")
 		fmt.Scan(&userTickets)
 
-		fmt.Printf("Enter your email: ")
-		fmt.Scan(&email)
+		if userTickets > remainingTickets {
+			fmt.Printf("Sorry %v, we only have %v tickets remaining.\n", firstName, remainingTickets)
+			continue
+		}
 
 		fmt.Printf("User %v has booked %v tickets.\n", firstName, userTickets)
 		fmt.Printf("Thank you %v %v for booking %v tickets. A confirmation email has been sent to %v.\n", firstName, lastName, userTickets, email)
@@ -31,8 +39,17 @@ func main() {
 		bookings = append(bookings, firstName+" "+lastName)
 		fmt.Printf("Tickets remaining: %v\n", remainingTickets)
 		fmt.Printf("The array langs: %v\n", len(bookings))
-		fmt.Printf("These are all the bookings: %v\n", bookings)
 
+		firstNames := []string{}
+		for _, booking := range bookings {
+			var names = strings.Fields(booking)
+			firstNames = append(firstNames, names[0])
+		}
+		fmt.Printf("The first names of the bookings are: %v\n", firstNames)
+		if remainingTickets == 0 {
+			fmt.Println("All tickets are sold out!")
+			break
+		}
 		//var bookings []string
 		//var bookings [50]string
 		//ask user for their name
@@ -41,4 +58,5 @@ func main() {
 		//fmt.Printf("Type of array: %T\n", bookings)
 		//fmt.Printf("The first value: %v\n", bookings[0])
 	}
+
 }
